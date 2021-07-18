fded993ad4b71feb05890446ab57c282ea933bcf
shixu <shixuqaq@163.com>
2021-07-18 18:54:27 +0800

    增加热部署，修改数据库配置，链接测试数据库

--- src/main/java/xyz/superinterface/daoshicha/DaoshichaApplication.java
+++ src/main/java/xyz/superinterface/daoshicha/DaoshichaApplication.java
@@ -1,10 +1,11 @@
 package xyz.superinterface.daoshicha;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DaoshichaApplication {
+public class DaoshichaApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DaoshichaApplication.class, args);
