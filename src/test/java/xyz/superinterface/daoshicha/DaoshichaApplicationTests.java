26a81eba24b776e2d239aa04ef77639bc573a37b
SuperInterface <su_jia_nan@163.com>
2021-07-18 15:22:08 +0800

    初始化项目

---
+++ src/test/java/xyz/superinterface/daoshicha/DaoshichaApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.superinterface.daoshicha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoshichaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
